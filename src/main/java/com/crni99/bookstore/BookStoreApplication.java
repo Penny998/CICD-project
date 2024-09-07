1dd6051096762383f2bc92d6d27c1110a59f30d5
Penny998 <74766255+Penny998@users.noreply.github.com>
2024-09-07 20:45:22 +0530

    Update BookStoreApplication.java

--- src/main/java/com/crni99/bookstore/BookStoreApplication.java
+++ src/main/java/com/crni99/bookstore/BookStoreApplication.java
@@ -2,12 +2,18 @@ package com.crni99.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(BookStoreApplication.class);
+	    }
 }
